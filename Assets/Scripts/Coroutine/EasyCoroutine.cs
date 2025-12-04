d70f88f112396095e3e3d688aa0daa9bb726c31f
Mirajes <myrrrclone@gmail.com>
2025-12-04 10:34:26 +0300

    coroutine +cd check

--- Assets/Scripts/Coroutine/EasyCoroutine.cs
+++ Assets/Scripts/Coroutine/EasyCoroutine.cs
@@ -11,20 +11,63 @@ public class EasyCoroutine : MonoBehaviour
     [SerializeField] private GameObject _freeze;
     [SerializeField] private GameObject _explosion;
 
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _fireballAudio;
+    [SerializeField] private AudioClip _freezeAudio;
+    [SerializeField] private AudioClip _explosionAudio;
+
+    private AblityData ablityData = new();
+
+    private void InitStructCuzUHaveVeryOldCSharp()
+    {
+        ablityData.CD_Fireball = 3f;
+        ablityData.CD_Freeze = 4f;
+        ablityData.CD_Explosion = 10f;
+    }
+
+    private void Start()
+    {
+        InitStructCuzUHaveVeryOldCSharp();
+    }
     private void Update()
     {
-        // можно сделать через inputSystem
+        // potom zamenit na inputSystem
         if (Input.GetKeyDown(KeyCode.Z)) { _fireballButton.onClick.Invoke(); }
         if (Input.GetKeyDown(KeyCode.X)) { _freezeButton.onClick.Invoke(); }
         if (Input.GetKeyDown(KeyCode.C)) { _explosionButton.onClick.Invoke(); }
 
-        {
 
+    }
+
+    private bool IsOnCooldown(float LT_used_Ability, float CD_Ability) => Time.time - LT_used_Ability >= CD_Ability;
+
+    public void FireballRelease()
+    {
+        if (IsOnCooldown(ablityData.LT_used_Fireball, ablityData.CD_Fireball)) {
+            ablityData.LT_used_Fireball = Time.time;
+            _audioSource.PlayOneShot(_fireballAudio);
+        }
+
+    }
+
+    public void FreezeRelease()
+    {
+        if (IsOnCooldown(ablityData.LT_used_Freeze, ablityData.CD_Freeze)) {
+            ablityData.LT_used_Freeze = Time.time;
+            _audioSource.PlayOneShot(_freezeAudio);
         }
     }
 
-    
+    public void ExplosionRelease()
+    {
+                if (IsOnCooldown(ablityData.LT_used_Explosion, ablityData.CD_Explosion)) {
+            ablityData.LT_used_Explosion = Time.time;
+            _audioSource.PlayOneShot(_explosionAudio);
+        }
+    }
 }
+
+
 
 public struct AblityData
 {
